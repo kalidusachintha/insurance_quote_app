a9a43e2fa2a7aa30ffbee6dacad77d57d7423385
kalidusachintha <kalidusachintha7@gmail.com>
2025-05-06 16:58:30 +0530

    Change service provider to add service and repository class bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\CoverageOption;
+use App\Models\Destination;
+use App\Models\Quote;
+use App\Repositories\Contracts\CoverageOptionsRepositoryInterface;
+use App\Repositories\Contracts\DestinationRepositoryInterface;
+use App\Repositories\Contracts\QuoteRepositoryInterface;
+use App\Repositories\CoverageOptionRepository;
+use App\Repositories\DestinationRepository;
+use App\Repositories\QuoteRepository;
+use App\Services\Pricing\Contracts\StandardQuoteInterface;
+use App\Services\Pricing\PricingStrategyFactory;
+use App\Services\QuoteService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +23,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(DestinationRepositoryInterface::class, function ($app) {
+            return new DestinationRepository($app->make(Destination::class));
+        });
+
+        $this->app->singleton(CoverageOptionsRepositoryInterface::class, function ($app) {
+            return new CoverageOptionRepository($app->make(CoverageOption::class));
+        });
+
+        $this->app->singleton(QuoteRepositoryInterface::class, function ($app) {
+            return new QuoteRepository($app->make(Quote::class));
+        });
+
+        $this->app->singleton(StandardQuoteInterface::class, function ($app) {
+            return new QuoteService(
+                $app->make(DestinationRepositoryInterface::class),
+                $app->make(CoverageOptionsRepositoryInterface::class),
+                $app->make(QuoteRepositoryInterface::class),
+                $app->make(PricingStrategyFactory::class)
+            );
+        });
     }
 
     /**
