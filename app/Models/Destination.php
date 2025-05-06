9c2575c34c3c2727857585fae7f2d89d928e7ed0
kalidusachintha <kalidusachintha7@gmail.com>
2025-05-06 11:17:11 +0530

    Add database migrations,models,seeders,factory

---
+++ app/Models/Destination.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Destination extends Model
+{
+    /** @use HasFactory<\Database\Factories\DestinationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'base_price',
+    ];
+
+    protected $casts = [
+        'base_price' => 'decimal:2',
+    ];
+
+    /** Get Quotes
+     * @return HasMany
+     */
+    public function quotes(): HasMany
+    {
+        return $this->hasMany(Quote::class);
+    }
+}
